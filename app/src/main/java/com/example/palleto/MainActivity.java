37b32e2b259355ea6ab6f5692363648ad5e53416
atenasadat <atenasaghi@yahoo.com>
2020-07-29 11:53:16 +0430

    FirstPage_Drawer added.

--- app/src/main/java/com/example/palleto/MainActivity.java
+++ app/src/main/java/com/example/palleto/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.palleto;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout mDrawelayout;
+    private ActionBarDrawerToggle mToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawelayout = findViewById(R.id.drawelayout);
+        mToggle = new ActionBarDrawerToggle(this , mDrawelayout ,R.string.open, R.string.close);
+        mDrawelayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         Boolean isfirstrun = getSharedPreferences("PEREFERENCE" , MODE_PRIVATE).getBoolean("isfirstrun" ,true );
 
@@ -18,7 +31,15 @@ public class MainActivity extends AppCompatActivity {
 
             Intent intent = new Intent(this , IntroduceActivity.class);
             startActivity(intent);
-//            getSharedPreferences("PEREFERENCE" , MODE_PRIVATE).edit().putBoolean("isfirstrun" , false).commit();
+            getSharedPreferences("PEREFERENCE" , MODE_PRIVATE).edit().putBoolean("isfirstrun" , false).commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
